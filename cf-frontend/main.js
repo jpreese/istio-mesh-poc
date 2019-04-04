bf7ddd19a2792a210f3fcd23d1c37921b60132a7
John Reese <john@reese.dev>
2019-04-04 14:05:49 -0400

    Mesh calling single service

--- cf-frontend/main.js
+++ cf-frontend/main.js
@@ -1,7 +1,7 @@
 const { CatFactRequest } = require('./proto/catfact_pb.js');
 const { CatFactServiceClient } = require('./proto/catfact_grpc_web_pb.js');
 
-var client = new CatFactServiceClient('http://localhost');
+var client = new CatFactServiceClient('http://localhost:7000');
 var factPlaceholder = document.getElementById('factPlaceholder');
 
 window.getFact = function() {
